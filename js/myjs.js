c6e185b1440ea2381d1693d06c3531f2d749cae7
zhangyachao <451142214@qq.com>
2017-04-05 16:55:35 +0800

    shengmingzhouqi

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,19 @@
 /**
  * Created by zhangyachao on 2017/4/5.
  */
+var vm = new Vue({
+    el:"#content",
+    data:{
+       title:"hello vue"
+    },
+   mounted: function () {
+            this.cartview();
+
+    },
+    methods:{
+        cartview: function(){
+            this.title = "qqqq";
+        }
+    }
+
+});
